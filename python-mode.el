bbbb03a54e5211f84cebe8c9028131d3a3aa715b
Barry Warsaw <barry@python.org>
2008-11-26 23:15:17 -0500

    Set version to 5.0a1

--- python-mode.el
+++ python-mode.el
@@ -9,7 +9,7 @@
 ;; Created:    Feb 1992
 ;; Keywords:   python languages oop
 
-(defconst py-version "$Revision$"
+(defconst py-version "5.0a1"
   "`python-mode' version number.")
 
 ;; This software is provided as-is, without express or implied
