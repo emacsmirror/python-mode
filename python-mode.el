8a09a83bd2ed8d7c0e695ad501b00aaceaac3b88
Georg Brandl <georg@python.org>
2011-01-09 08:27:27 +0100

    Disable special marking for string escapes for now; it causes unexplained font-locking failures.

--- python-mode.el
+++ python-mode.el
@@ -649,25 +649,25 @@ support for features needed by `python-mode'.")
      '("XXX\\|TODO\\|FIXME" 0 py-XXX-tag-face t)
      ;; special marking for string escapes and percent substitutes;
      ;; loops adapted from lisp-mode in font-lock.el
-     '((lambda (bound)
-         (catch 'found
-           (while (re-search-forward
-                   (concat
-                    "\\(\\\\\\\\\\|\\\\x..\\|\\\\u....\\|\\\\U........\\|"
-                    "\\\\[0-9][0-9]*\\|\\\\[abfnrtv\"']\\)") bound t)
-             (let ((face (get-text-property (1- (point)) 'face)))
-               (when (or (and (listp face) (memq 'font-lock-string-face face))
-                         (eq 'font-lock-string-face face))
-                 (throw 'found t))))))
-       (1 'font-lock-regexp-grouping-backslash prepend))
-     '((lambda (bound)
-         (catch 'found
-           (while (re-search-forward "\\(%[^(]\\|%([^)]*).\\)" bound t)
-             (let ((face (get-text-property (1- (point)) 'face)))
-               (when (or (and (listp face) (memq 'font-lock-string-face face))
-                         (eq 'font-lock-string-face face))
-                 (throw 'found t))))))
-       (1 'font-lock-regexp-grouping-construct prepend))
+     ;; '((lambda (bound)
+     ;;     (catch 'found
+     ;;       (while (re-search-forward
+     ;;               (concat
+     ;;                "\\(\\\\\\\\\\|\\\\x..\\|\\\\u....\\|\\\\U........\\|"
+     ;;                "\\\\[0-9][0-9]*\\|\\\\[abfnrtv\"']\\)") bound t)
+     ;;         (let ((face (get-text-property (1- (point)) 'face)))
+     ;;           (when (or (and (listp face) (memq 'font-lock-string-face face))
+     ;;                     (eq 'font-lock-string-face face))
+     ;;             (throw 'found t))))))
+     ;;   (1 'font-lock-regexp-grouping-backslash prepend))
+     ;; '((lambda (bound)
+     ;;     (catch 'found
+     ;;       (while (re-search-forward "\\(%[^(]\\|%([^)]*).\\)" bound t)
+     ;;         (let ((face (get-text-property (1- (point)) 'face)))
+     ;;           (when (or (and (listp face) (memq 'font-lock-string-face face))
+     ;;                     (eq 'font-lock-string-face face))
+     ;;             (throw 'found t))))))
+     ;;   (1 'font-lock-regexp-grouping-construct prepend))
      ))
   "Additional expressions to highlight in Python mode.")
 
