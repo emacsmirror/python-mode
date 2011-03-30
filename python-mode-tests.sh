36acf2c3dfe80ced264301dde75a2240310aae0c
Andreas Roehler <andreas.roehler@online.de>
2011-03-30 08:21:51 +0200

    indent-after-return-lp:745208 test added

--- python-mode-tests.sh
+++ python-mode-tests.sh
@@ -54,7 +54,8 @@ $HOME/emacs/src/emacs -Q --batch --eval "(message (emacs-version))" --eval "(whe
 --funcall goto-beginning-of-tqs-lp:735328-test \
 --funcall class-treated-as-keyword-lp:709478-test \
 --funcall backslashed-continuation-line-indent-lp:742993-test \
---funcall py-decorators-face-lp:744335-test
+--funcall py-decorators-face-lp:744335-test \
+--funcall indent-after-return-lp:745208-test
 
 # fail as batch-skripts, but work from inside
 # --funcall mark-block-region-lp:328806-test \
