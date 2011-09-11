ea897833bd71f97a27784cf3938dfdc42e40713f
Andreas Roehler <andreas.roehler@online.de>
2011-09-11 18:03:35 +0200

    deliver access to probably installed shells instead of generating

--- python-mode.el
+++ python-mode.el
@@ -2124,6 +2124,59 @@ interpreter.
     (use-local-map py-shell-map)
     (run-hooks 'py-shell-hook)))
 
+(defun python (&optional argprompt)
+  "Start an interactive Python interpreter in another window.
+  With optional \\[universal-argument] user is prompted
+    for options to pass to the Python interpreter.
+    "
+  (interactive)
+  (let ((py-shell-name "python"))
+    (py-shell argprompt)))
+
+(defun jython (&optional argprompt)
+  "Start an interactive Jython interpreter in another window.
+  With optional \\[universal-argument] user is prompted
+    for options to pass to the Jython interpreter.
+    "
+  (interactive)
+  (let ((py-shell-name "jython"))
+    (py-shell argprompt)))
+
+(defun ipython (&optional argprompt)
+  "Start an interactive Ipython interpreter in another window.
+   With optional \\[universal-argument] user is prompted
+    for options to pass to the Ipython interpreter. "
+  (interactive)
+  (let ((py-shell-name "ipython"))
+    (py-shell argprompt)))
+
+(defun python2 (&optional argprompt)
+  "Start an interactive Python2 interpreter in another window.
+  With optional \\[universal-argument] user is prompted
+    for options to pass to the Python2 interpreter.
+    "
+  (interactive)
+  (let ((py-shell-name "python2"))
+    (py-shell argprompt)))
+
+(defun python3 (&optional argprompt)
+  "Start an interactive Python3 interpreter in another window.
+  With optional \\[universal-argument] user is prompted
+    for options to pass to the Python3 interpreter.
+    "
+  (interactive)
+  (let ((py-shell-name "python3"))
+    (py-shell argprompt)))
+
+(defun python2.7 (&optional argprompt)
+  "Start an interactive Python2.7 interpreter in another window.
+  With optional \\[universal-argument] user is prompted
+    for options to pass to the Python2.7 interpreter.
+    "
+  (interactive)
+  (let ((py-shell-name "python2.7"))
+    (py-shell argprompt)))
+
 (defun py-clear-queue ()
   "Clear the queue of temporary files waiting to execute."
   (interactive)
