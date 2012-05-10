31fc86448307d60eb86155d816833621074ae569
Andreas Roehler <andreas.roehler@online.de>
2012-05-10 13:19:31 +0200

    remaining patch applied, lp:328803
    Thanks!

--- completion/pycomplete.el
+++ completion/pycomplete.el
@@ -62,8 +62,8 @@
   (let* ((pymacs-forget-mutability t)
          (symbol (py-symbol-near-point))
          (completions
-          (list (pycomplete-pycomplete symbol
-                                       (py-find-global-imports)))))
+          (pycomplete-pycomplete symbol
+                                 (py-find-global-imports))))
     (cond ((null completions) ; no matching symbol
            (message "Can't find completion for \"%s\"" symbol)
            (ding))
