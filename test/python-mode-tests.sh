6c5766e15cd5f017835e0be45354c34db81743c4
Andreas Roehler <andreas.roehler@online.de>
2012-05-10 10:28:37 +0200

    Pymacs python-mode.el difficulty, lp:996969, fixed

--- test/python-mode-tests.sh
+++ test/python-mode-tests.sh
@@ -1092,8 +1092,8 @@ $EMACS -Q --batch --eval "(message (emacs-version))" --eval "(when (featurep 'py
 --funcall usr-bin-ipython-shell-complete-test \
 \
 --funcall py-ipython-complete-lp:927136-test \
---funcall py-describe-symbol-fails-on-modules-lp:919719-test \
 --funcall py-end-of-print-statement-test \
+--funcall py-describe-symbol-fails-on-modules-lp:919719-test \
 --funcall py-menu-pyshell-test \
 --funcall split-windows-on-execute-p-test \
 --funcall script-buffer-appears-instead-of-python-shell-buffer-lp:957561-test \
