311f7d6f4904652bc869687cc61b0c83b79ad117
Andreas Röhler <andreas.roehler@online.de>
2014-03-27 15:10:19 +0100

    Report state of python-mode variables , lp:1295002
    fixed
    
    New helper command variables-state:
    Diplays state of python-mode variables in an org-mode buffer 
    
    Variables which would produce a large output are left out:
    - syntax-tables
    - python-mode-map
    
    Unused variable `py-space-backslash-table' removed
    Docu updated

--- python-mode.el
+++ python-mode.el
@@ -1600,9 +1600,9 @@ SYMMETRIC:
   :group 'python-mode)
 (defvar py-exception-name-face 'py-exception-name-face)
 
-(defvar virtualenv-old-path)
+(defvar virtualenv-old-path nil)
 
-(defvar virtualenv-old-exec-path)
+(defvar virtualenv-old-exec-path nil)
 
 (defvar py-underscore-word-syntax-p t
   "This is set later by defcustom, only initial value here.
@@ -3654,14 +3654,6 @@ of current line."
 
 
 ;;;
-(defconst py-space-backslash-table
-  (let ((table (copy-syntax-table python-mode-syntax-table)))
-    (modify-syntax-entry ?\\ " " table)
-    table)
-  "`python-mode-syntax-table' with backslash given whitespace syntax.")
-
-;; have to bind py-file-queue before installing the kill-emacs-hook
-
 (define-abbrev-table 'python-mode-abbrev-table ())
 
 (define-abbrev-table 'inferior-python-mode-abbrev-table ())
@@ -11055,6 +11047,70 @@ Sends help if stuff is missing. "
   (if (functionp 'pydoc-info-add-help)
       (call-interactively 'info-lookup-symbol)
     (message "pydoc-info-add-help not found. Please check INSTALL-INFO-FILES")))
+
+;;; 
+(defun variables-state (&optional buffer directory-in directory-out)
+  "Diplays state of python-mode variables in an org-mode buffer.
+
+Reads variables from python-mode.el as current buffer.
+
+Variables which would produce a large output are left out:
+- syntax-tables
+- python-mode-map
+
+Maybe call M-x describe-variable RET to query its value. "
+  (interactive)
+  (variables-prepare "state"))
+
+(defun variables-prepare (kind)
+  "Used by variable-finds, variable-states. "
+  (let* ((oldbuf (buffer-name (or buffer (current-buffer))))
+         ;; (file (buffer-file-name))
+         (orgname (concat (substring oldbuf 0 (string-match "\\." oldbuf)) ".org"))
+         (reSTname (concat (substring oldbuf 0 (string-match "\\." oldbuf)) ".rst"))
+         (directory-in (or directory-in (and (not (string= "" py-devel-directory-in)) py-devel-directory-in) default-directory))
+         (directory-out (or directory-out (expand-file-name finds-directory-out)))
+	 (command (concat "variables-base-" kind)))
+    (funcall (intern-soft command) oldbuf orgname reSTname directory-in directory-out)))
+
+(defun variables-base-state (oldbuf orgname reSTname directory-in directory-out)
+  (save-restriction
+    (let ((suffix (file-name-nondirectory (buffer-file-name)))
+          variableslist)
+      ;; (widen)
+      (goto-char (point-min))
+      ;; (eval-buffer)
+      (while (and (not (eobp))(re-search-forward "^(defvar [[:alpha:]]\\|^(defcustom [[:alpha:]]\\|^(defconst [[:alpha:]]" nil t 1))
+        (let* ((name (symbol-at-point))
+               (state
+                (unless
+                    (or (eq name 'py-menu)
+                        (eq name 'python-mode-map)
+                        (string-match "syntax-table" (prin1-to-string name)))
+
+                  (prin1-to-string (symbol-value name)))))
+          (if state
+              (add-to-list 'variableslist (cons (prin1-to-string name) state))
+            (message "don't see a state for %s" (prin1-to-string name))))
+        (forward-line 1))
+      (setq variableslist (nreverse variableslist))
+      ;; (with-temp-buffer
+      (set-buffer (get-buffer-create "State-of-Python-mode-variables.org"))
+      (erase-buffer)
+      ;; org
+      (insert "State of python-mode variables\n\n")
+      (switch-to-buffer (current-buffer))
+      (dolist (ele variableslist)
+        (if (string-match "^;;; " (car ele))
+            (unless (or (string-match "^;;; Constants\\|^;;; Commentary\\|^;;; Code\\|^;;; Macro definitions\\|^;;; Customization" (car ele)))
+
+              (insert (concat (replace-regexp-in-string "^;;; " "* " (car ele)) "\n")))
+          (insert (concat "\n** "(car ele) "\n"))
+          (insert (concat "   " (cdr ele) "\n\n")))
+        (richten)
+        (sit-for 0.01))
+      (sit-for 0.01)
+      (org-mode))))
 
 ;;;
 (defun py-send-receive (string)
