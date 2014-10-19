12eee21b8be73763623c29e62e2c7710dd7e8d70
Andreas Röhler <andreas.roehler@online.de>
2014-10-19 21:10:31 +0200

    `py-indent-line' lp:1382799
    fixed

--- python-mode.el
+++ python-mode.el
@@ -6230,10 +6230,7 @@ Unclosed-string errors are not handled here, as made visible by fontification al
 Returns position reached if point was moved. "
   (let ((orig (point)))
     (and (< 0 (abs (skip-chars-backward "^;" (or limit (line-beginning-position)))))
-	 ;; (if (eq ?\; (char-after))
-	 ;; (skip-chars-forward ";" (line-end-position))
-	   (skip-chars-forward " \t" (line-beginning-position))
-	   ;; )
+	 (skip-chars-forward " \t" (line-end-position))
 	 (setq done t)
 	 (and (< (point) orig) (point)))))
 
@@ -6249,15 +6246,14 @@ Returns position reached if point was moved. "
              (regexp (or regexp 'py-extended-block-or-clause-re))
              (thisregexp
               (cond ((eq regexp 'py-def-or-class-re)
-                     (concat "\\|" py-def-or-class-re))
+                     py-def-or-class-re)
                     ((eq regexp 'py-def-re)
-                     (concat "\\|" py-def-re))
-                    ((eq regexp 'py-class-re)
-                     (concat "\\|" py-class-re))
-                    ((eq regexp 'py-minor-block-re)
-                     py-minor-block-re)
-                    (t (concat "\\|" py-extended-block-or-clause-re))))
-
+                     py-def-re)
+		    ((eq regexp 'py-class-re)
+		     py-class-re)
+		    ((eq regexp 'py-minor-block-re)
+		     py-minor-block-re)
+		    (t py-extended-block-or-clause-re)))
              bofst
              (this (progn (back-to-indentation)
                           (setq bofst (py--beginning-of-statement-p))
@@ -6276,7 +6272,7 @@ Returns position reached if point was moved. "
         (cond (this
                (setq thisindent (current-indentation))
                (cond ((and py-close-provides-newline
-                           (or (eq thisregexp 'py-def-re)(eq thisregexp 'py-class-re)(eq thisregexp 'py-def-or-class-re)))
+                           (or (eq regexp 'py-def-re)(eq regexp 'py-class-re)(eq regexp 'py-def-or-class-re)))
                       (while
                           (and
                            ;; lp:1294478 py-mark-def hangs
@@ -6300,8 +6296,8 @@ Returns position reached if point was moved. "
                             (and (py-down-statement)
                                  (or (< thisindent (current-indentation))
                                      (and (eq thisindent (current-indentation))
-                                          (or (eq thisregexp 'py-minor-block-re)
-                                              (eq thisregexp 'py-block-re))
+                                          (or (eq regexp 'py-minor-block-re)
+                                              (eq regexp 'py-block-re))
                                           (looking-at py-clause-re)))
                                  (py-end-of-statement)(setq last (point))))
                         (and last (goto-char last)))))
@@ -6825,6 +6821,8 @@ http://docs.python.org/reference/compound_stmts.html"
                          (parse-partial-sexp (or limit (point-min))(point))))
              (done done)
              erg)
+	;; lp:1382788
+	(unless done (skip-chars-backward " \t\r\n\f"))
         (cond
          ((and (bolp)(eolp))
           (skip-chars-backward " \t\r\n\f")
@@ -6861,29 +6859,33 @@ http://docs.python.org/reference/compound_stmts.html"
             (setq done t))
           (back-to-indentation)
           (py-beginning-of-statement orig done limit))
-         ((and (not (eq (point) orig))(looking-back "^[ \t]*"))
-          (setq erg (point)))
-         ((and (not done) (not (eq 0 (skip-chars-backward " \t\r\n\f"))))
+	 ((and (not done) (eq (char-before) ?\;))
+	  (skip-chars-backward ";")
+	  (py-beginning-of-statement orig done limit))
+	 ((and (not done) (py--skip-to-semicolon-backward (save-excursion (back-to-indentation)(point))))
+	  ;; (back-to-indentation)
+	  (py-beginning-of-statement orig done limit))
+	 ((and (not done) (not (eq 0 (skip-chars-backward " \t\r\n\f"))))
           ;; (setq done t)
           (py-beginning-of-statement orig done limit))
-	 ((unless done)
-	  (and (py--skip-to-semicolon-backward (save-excursion (back-to-indentation)(point)))
-	       (back-to-indentation)
-	       (not (bobp))
-	       (py-beginning-of-statement orig done limit)))
-         ((and (not done)(not (eq (current-column) (current-indentation))))
-          (if (< 0 (abs (skip-chars-backward "^\t\r\n\f" (line-beginning-position))))
-              (progn
-                (setq done t)
-                (back-to-indentation)
-                (py-beginning-of-statement orig done limit))
-            (back-to-indentation)
-            (setq done t)
-            (py-beginning-of-statement orig done limit))))
-        (unless (and (looking-at "[ \t]*#") (looking-back "^[ \t]*"))
-          (when (< (point) orig)(setq erg (point))))
-        (when (and py-verbose-p (interactive-p)) (message "%s" erg))
-        erg))))
+	 ((and (not done)(not (eq (current-column) (current-indentation))))
+	  (if (ignore-errors
+		(< 0
+		   (abs
+		    (py--skip-to-semicolon-backward (save-excursion (back-to-indentation)(point))))
+		   ;; (skip-chars-backward "^\t\r\n\f" (line-beginning-position))
+))
+	      (progn
+		(setq done t)
+		;; (back-to-indentation)
+		(py-beginning-of-statement orig done limit))
+	    (back-to-indentation)
+	    (setq done t)
+	    (py-beginning-of-statement orig done limit))))
+	(unless (and (looking-at "[ \t]*#") (looking-back "^[ \t]*"))
+	  (when (< (point) orig)(setq erg (point))))
+	(when (and py-verbose-p (interactive-p)) (message "%s" erg))
+	erg))))
 
 (defalias 'py-backward-declarations 'py-beginning-of-declarations)
 (defun py-beginning-of-declarations ()
@@ -7625,7 +7627,7 @@ Optional argument REPEAT, the number of loops done already, is checked for py-ma
        ((nth 3 pps)
 	(when (py-end-of-string)
 	  (end-of-line)
-	  (skip-chars-backward " \t\r\n\f") 
+	  (skip-chars-backward " \t\r\n\f")
 	  (setq pps (parse-partial-sexp (point-min) (point)))
 	  (unless (and done (not (or (nth 1 pps) (nth 8 pps))) (eolp)) (py-end-of-statement orig done repeat))))
        ;; in non-terminated string
