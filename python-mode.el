893abd0704e279909549c8b1785bedf24a53334d
Andreas Röhler <andreas.roehler@online.de>
2015-07-12 17:07:35 +0200

    `py--fill-docstring-base', hand over orig

--- python-mode.el
+++ python-mode.el
@@ -17058,7 +17058,7 @@ See lp:1066489 "
   (py-indent-region docstring thisend)
   (goto-char orig))
 
-(defun py--fill-docstring-base (thisbeg thisend style multi-line-p first-line-p beg end py-current-indent)
+(defun py--fill-docstring-base (thisbeg thisend style multi-line-p first-line-p beg end py-current-indent orig)
   ;; (widen)
   ;; fill-paragraph causes wrong indent, lp:1397936
   ;; (narrow-to-region thisbeg thisend)
@@ -17106,7 +17106,7 @@ See lp:1066489 "
   (when multi-line-p
     ;; adjust the region to fill according to style
     (goto-char end)
-    (py--fill-docstring-base thisbeg thisend style multi-line-p first-line-p beg end py-current-indent))
+    (py--fill-docstring-base thisbeg thisend style multi-line-p first-line-p beg end py-current-indent orig))
   (goto-char orig))
 
 (defun py--fill-docstring-first-line ()
@@ -17162,7 +17162,7 @@ See lp:1066489 "
            (py--fill-docstring-last-line thisbeg thisend beg end style))
           (t ;; (narrow-to-region beg end)
 	     (fill-region beg end justify)))
-    (py--fill-docstring-base thisbeg thisend style multi-line-p first-line-p beg end py-current-indent)))
+    (py--fill-docstring-base thisbeg thisend style multi-line-p first-line-p beg end py-current-indent orig)))
 
 (defun py-fill-string (&optional justify style docstring)
   "String fill function for `py-fill-paragraph'.
