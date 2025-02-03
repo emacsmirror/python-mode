1425b2dd8dc221978fab918c2481dcfc2286996b
Andreas Roehler <andreas.roehler@online.de>
2025-02-03 12:25:33 +0100

    ‘py-execute-string’ fixed

--- python-mode.el
+++ python-mode.el
@@ -5347,7 +5347,7 @@ With optional Arg OUTPUT-BUFFER specify output-buffer"
       (if (eq 1 (length (window-list)))
           (py--shell-manage-windows buffer)
         (when (get-register py--windows-config-register)
-          (jump-to-register (get-register py--windows-config-register)))))))
+          (ignore-errors (jump-to-register (get-register py--windows-config-register))))))))
 
 (defun py--shell-manage-windows (output-buffer &optional exception-buffer split switch)
   "Adapt or restore window configuration from OUTPUT-BUFFER.
@@ -9684,6 +9684,103 @@ START END SHELL FILENAME PROC FILE WHOLEBUF FAST DEDICATED SPLIT SWITCH."
     (py--execute-base-intern strg filename proc wholebuf py-output-buffer origline execute-directory start end fast)))
 
 ;; python-components-execute-file
+
+(defun py-execute-file-ipython (filename)
+  "Send file to IPython interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil nil "ipython" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-ipython3 (filename)
+  "Send file to IPython3 interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil nil "ipython3" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-jython (filename)
+  "Send file to Jython interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil nil "jython" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-python (filename)
+  "Send file to Python interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil nil "python" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-python2 (filename)
+  "Send file to Python2 interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil nil "python2" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-python3 (filename)
+  "Send file to Python3 interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil nil "python3" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-pypy (filename)
+  "Send file to PyPy interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil nil "pypy" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file- (filename)
+  "Send file to  interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil nil "" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-ipython-dedicated (filename)
+  "Send file to a dedicatedIPython interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil t "ipython" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-ipython3-dedicated (filename)
+  "Send file to a dedicatedIPython3 interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil t "ipython3" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-jython-dedicated (filename)
+  "Send file to a dedicatedJython interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil t "jython" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-python-dedicated (filename)
+  "Send file to a dedicatedPython interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil t "python" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-python2-dedicated (filename)
+  "Send file to a dedicatedPython2 interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil t "python2" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-python3-dedicated (filename)
+  "Send file to a dedicatedPython3 interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil t "python3" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file-pypy-dedicated (filename)
+  "Send file to a dedicatedPyPy interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil t "pypy" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
+(defun py-execute-file--dedicated (filename)
+  "Send file to a dedicated interpreter"
+  (interactive "fFile: ")
+  (let ((buffer (py-shell nil nil t "" nil t)))
+    (py--execute-file-base filename (get-buffer-process buffer) nil buffer nil t)))
+
 ;; python-components-up
 
 
@@ -25990,13 +26087,6 @@ may want to re-add custom functions to it using the
   (compilation-shell-minor-mode 1)
   (add-hook 'completion-at-point-functions
 	    #'py-shell-completion-at-point nil 'local)
-  ;; (cond
-  ;;  ((string-match "^[Jj]" (process-name (get-buffer-process (current-buffer))))
-  ;;   'indent-for-tab-command)
-  ;;  (t
-  ;;   ;; (define-key py-shell-mode-map "\t"
-  ;;   ;;     	'py-indent-line)
-  ;;   ))
   (define-key py-shell-mode-map [(control c) (control r)] 'py-nav-last-prompt)
   (make-local-variable 'py-pdbtrack-buffers-to-kill)
   (make-local-variable 'py-shell-fast-last-output)
